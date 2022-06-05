7d6fd95b08e15fa5a917d1fe1db023b78f7c0ea2
durgadeepak <104149761+durgadeepak@users.noreply.github.com>
2022-06-05 13:38:15 +0530

    created positive numbers.py

---
+++ positive numbers.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[50]:
+
+
+list1 = [12,-7,5,64,-14]
+list2 = [12,14,-95,3]
+list=[]
+for num in list1:
+ if num>=0:
+    print("\r", num)
+for num in list2:
+  if num>=0:
+    list.append(num)
+print(list)
+    
+   
+
+
+# In[ ]:
+
+
+
+
